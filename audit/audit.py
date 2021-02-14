3ad3dab73a013e309df44ad2f9737f6fcde56a4f
Keaton <46245956+keatonjs@users.noreply.github.com>
2021-02-13 23:23:25 -0500

    Update audit.py

--- audit/audit.py
+++ audit/audit.py
@@ -97,7 +97,7 @@ class Audit(commands.Cog):
         self.invite_regex = re.compile(
             r"(?:https?://)?(?:www\.)?(?:discord\.(?:gg|io|me|li)|(?:discordapp|discord)\.com/invite)/[\w]+"
         )
-        self.whname = "Modmail Audit Logger"
+        self.whname = "Beneath Audit"
         self.acname = "modmail-audit"
         self._webhooks = {}
         self._webhook_locks = {}
